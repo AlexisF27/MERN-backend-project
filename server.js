e034aa05b1ddd422f32e77a48fb32c9ead79aab6
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-26 20:10:15 +0000

    Logs middleware

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require('express')
 const app = express()
+const { logger } = require('./middleware/logger')
+
 const path = require('path')
 const PORT = process.env.PORT || 3500
 
-app.use('/', express.static(path.join(__dirname, '/public')))
+app.use(logger)
+app.use(express.json())
+
+
+app.use('/', express.static(path.join(__dirname, 'public')))
 app.use('/', require('./routes/root'))
 
 app.all('*', (req, res) => {
