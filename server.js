435902f316f678b7b3df809692fa24fa920284e9
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-26 14:32:20 +0000

    User stories

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express')
 const app = express()
-const path = require('express')
-const PORT = require('express')
+const app = require('path')
+const path = process.env.PORT || 3500
+
+app.use('/', express)
+app.listen(po)
