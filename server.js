ee79aca4a1ec49be669225d54fd75d6750366f87
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-02-26 14:42:43 +0000

    Server runing

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const app = require('path')
-const path = process.env.PORT || 3500
+const path = require('path')
+const PORT = process.env.PORT || 3500
 
-app.use('/', express)
-app.listen(po)
+app.use('/', express.static(path.join(__dirname, '/public')))
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
